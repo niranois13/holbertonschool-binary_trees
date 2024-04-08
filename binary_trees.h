61abd92f5cc5ce3cbd94923f5207e7237d77be85
niranois13 <8784@holbertonstudents.com>
2024-04-08 13:23:27 +0200

    Adding print function and 0-binary_tree_node.c

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,8 @@
+#ifndef __BINARY_TREES__
+#define __BINARY_TREES__
+#include <stddef.h>
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -6,12 +11,16 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+
+#endif /* __BINARY_TREES__ */
