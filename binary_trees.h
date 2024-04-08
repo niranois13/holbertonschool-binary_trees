59ee619825118cb43e48fbdee8706599b6feb5db
HolbieWan <8841@holbertonstudents.com>
2024-04-08 14:02:01 +0000

    Updated binary_trees.f

--- binary_trees.h
+++ binary_trees.h
@@ -13,15 +13,16 @@
  */
 typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 } binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 
 #endif /* __BINARY_TREES__ */
