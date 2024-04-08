08373413781758de44ae118ae69525fec56d7e4e
niranois13 <8784@holbertonstudents.com>
2024-04-08 21:50:26 +0200

    adding 8_postorder and all protypes

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_postorder - function that that goes through a binary list using
+*							postorder traversal
+* @tree: pointer to the root of the binary tree to traverse
+* @func: pointer to a function that prints content of each node traversed
+* Return: void
+*/
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	else
+	{
+		binary_tree_postorder(tree->right, func);
+		binary_tree_postorder(tree->left, func);
+		func(tree->n);
+	}
+}
