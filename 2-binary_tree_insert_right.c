f0f3a5357771c306878177ab49ca98ee51b42641
niranois13 <8784@holbertonstudents.com>
2024-04-08 18:41:02 +0200

    adding reworked insert_right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,7 +8,12 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
@@ -20,16 +25,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 
 	if (parent->right != NULL)
-    {
-        new_node->right = parent->right;
-        parent->right->parent = new_node;
-        parent->right = new_node;
-    }
-	else
 	{
-		free(new_node);
-		return (NULL);
+		new_node->right = parent->right;
+		new_node->right->parent = new_node;
+
 	}
+	parent->right = new_node;
 
 	return (new_node);
 }
