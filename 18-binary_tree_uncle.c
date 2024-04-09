eac754eb10f81731df660461de38e302d724f4ab
HolbieWan <8841@holbertonstudents.com>
2024-04-09 12:21:02 +0000

    Updated 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -13,11 +13,13 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	}
 
 	if (node->parent->parent->left != NULL
-	&& node->parent->parent->right == node->parent)
+	&& node->parent->parent->right != NULL
+	&& node->parent->parent->left != node->parent )
 		return (node->parent->parent->left);
 
-	if (node->parent->parent->right != NULL
-	&& node->parent->parent->left == node->parent)
+	else if (node->parent->parent->right != NULL
+	&& node->parent->parent->left != NULL
+	&& node->parent->parent->right != node->parent )
 		return (node->parent->parent->right);
 
 	else
