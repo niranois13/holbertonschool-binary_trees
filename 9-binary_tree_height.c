dae527f971bd77fce3a0096b67f442acd7ab9793
niranois13 <8784@holbertonstudents.com>
2024-04-09 02:06:20 +0200

    refonte de 9-binary_tree_height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,20 +12,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_left, height_right;
 
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return 0;
+		return (0);
 
 	height_left = binary_tree_height(tree->left);
 	height_right = binary_tree_height(tree->right);
 
 	if (height_left > height_right)
 	{
-		return height_left + 1;
+		return (height_left + 1);
 	}
 	else
 	{
-		return height_right + 1;
+		return (height_right + 1);
 	}
 }
