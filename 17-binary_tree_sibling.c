a91d7703ee230dbee8bc9b95c352516570463eb7
HolbieWan <8841@holbertonstudents.com>
2024-04-09 11:34:20 +0000

    Updated file: 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,17 +3,13 @@
 /**
 * binary_tree_sibling - function that finds the sibling of a node
 * @node: a pointer to the node to find the sibling
-* @func: function that will print data cointained by each node traversed
+*
 * Return:  a pointer to the sibling node
 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL)
-		return (NULL);
-
-	if (node->parent == NULL)
+	if (node == NULL || node->parent == NULL)
 	{
-		printf("nil");
 		return (NULL);
 	}
 
