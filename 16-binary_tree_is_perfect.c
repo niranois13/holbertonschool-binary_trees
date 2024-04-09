fc485904b943bf06fe4b3f3f3f3c3108aa4b4207
niranois13 <8784@holbertonstudents.com>
2024-04-09 12:04:24 +0200

    removed depth calculation of 16-binary_tree_is_perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -4,7 +4,7 @@
 * @tree: pointer to the root of the binary tree
 * Return: number of leaves
 */
-/*
+
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leaves = 0;
@@ -24,13 +24,13 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	return (leaves);
 }
-*/
+
 /**
 * binary_tree_height - function that measures the height of a binary tree
 * @tree: pointer to the binary tree
 * Return: balance factor of the binary tree
 */
-/*
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height_left, height_right;
@@ -53,30 +53,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (height_right + 1);
 	}
 }
-*/
-/**
-* binary_tree_depth - function that prints th depth of nodes in a binary tree
-* @tree: poiter to the binary tree
-* Return: size of the depth of the node checked
-*/
-size_t binary_tree_depth(const binary_tree_t *tree)
-{
-	size_t depth = 0;
-
-	if (tree == NULL)
-	{
-		return (0);
-	}
-
-	while (tree->parent != NULL)
-	{
-		tree = tree->parent;
-		depth++;
-	}
-
-	return (depth);
-}
-
 
 /**
 * binary_tree_is_perfect - function that checks if a binary tree is perfect
@@ -85,26 +61,23 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t depth;
-	/*size_t height, leaves, isperfect;*/
+	/*size_t depth;*/
+	size_t height, leaves, isperfect;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 
-	depth = binary_tree_depth(tree);
+	/*depth = binary_tree_depth(tree);
 	printf("depth = %zu\n", depth);
-	return (depth);
-}
-	/*height = binary_tree_height(tree);
-	printf("height = %zu\n", height);
-	leaves = binary_tree_leaves(tree);
-	printf("leaves = %zu\n", leaves);
-	isperfect = (1 << height) - 1;
-	printf("isperfect = %zu\n", isperfect);
+	return (depth);*/
 
-	if (leaves == isperfect && depth == height)
+	height = binary_tree_height(tree);
+	leaves = binary_tree_leaves(tree);
+	isperfect = (1 << height);
+
+	if (leaves == isperfect)
 		return (1);
 	else
 		return (0);
 }
-*/
+
