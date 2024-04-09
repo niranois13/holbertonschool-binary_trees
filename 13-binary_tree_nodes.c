be8430ff81295a2a1845a398df257e2facbc469c
niranois13 <8784@holbertonstudents.com>
2024-04-09 15:06:24 +0200

    adding comment blocs for 13-binary_tree_nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 size_t binary_tree_size(const binary_tree_t *tree);
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree);
 
 /**
 * binary_tree_nodes - function that counts the nodes with
