2e3e21cf191174e24decd76e902bb1f828f353fd
niranois13 <8784@holbertonstudents.com>
2024-04-09 14:23:47 +0200

    adding 18-binary_tree_uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,21 +7,22 @@
 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 	{
 		return (NULL);
 	}
 
-	if (node->parent->parent->left != NULL
-	&& node->parent->parent->right != NULL
-	&& node->parent->parent->left != node->parent )
+	if (node->parent->parent->left != node->parent)
+	{
 		return (node->parent->parent->left);
-
-	else if (node->parent->parent->right != NULL
-	&& node->parent->parent->left != NULL
-	&& node->parent->parent->right != node->parent )
-		return (node->parent->parent->right);
-
+	}
+	else if (node->parent->parent->right == node->parent)
+	{
+		return (node->parent);
+	}
 	else
+	{
 		return (NULL);
+	}
+	return (0);
 }
